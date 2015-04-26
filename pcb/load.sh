2a0e345e9753775cc07f0e4d123aae6a82d1ab9c
solarfiretech <tim.gack@solarfiretech.com>
2015-04-26 14:18:07 -0500

    Added captured schematics and support scripts.

---
+++ pcb/load.sh
@@ -0,0 +1,2 @@
+gschem sft-hw-0026-1.sch sft-hw-0026-2.sch
+
---
+++ pcb/refdes.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# 
+# Copyright (c) 2015
+# Solarfire Technologies, LLC
+#
+# Contact: tim.gack@solarfiretech.com
+#
+# Licensed under CERN OHL v.1.2 or later
+#
+# You may redistribute and modify this documentation under the terms of the
+# CERN OHL v.1.2. (http://ohwr.org/cernohl) or later . This documentation is 
+# distributed WITHOUT ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING OF
+# MERCHANTABILITY, SATISFACTORY QUALITY AND FITNESS FOR A PARTICULAR 
+# PURPOSE. Please see the CERN OHL v.1.2 or later for applicable conditions.
+#
+
+# Renumbers all reference designators that have not 
+# already been assinged
+refdes_renum --gentle --verbose sft-hw-0026-1.sch  sft-hw-0026-2.sch
